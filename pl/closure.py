34450d87fefee9c8bf32263b419aecd9a2ff5d21
alexodisseus <alexodisseus@gmail.com>
2023-11-23 07:12:52 -0300

    comentario

--- pl/closure.py
+++ pl/closure.py
@@ -41,6 +41,38 @@ def index():
 	return render_template('closure/index.html' , data=data , pagination = pagination)
 	#return render_template('login.html' )
 
+
+@closure.route('/listar', methods = ['GET','POST'])
+def list():
+	"""
+	if 'username' not in session:
+		return redirect(url_for('admin.login'))
+
+	"""
+
+
+	filters = request.args.get('search')
+	page = request.args.get('page', 1, type=int)
+	per_page = 10
+	offset = (page - 1) * per_page
+
+	
+	data = model.list_closure(filters, offset, per_page )
+	
+	count = model.count_closure(filters, offset, per_page )
+
+	count = math.ceil(count/ per_page)
+	pagination =[
+	filters,
+	page,
+	offset,
+	per_page,
+	count
+	]
+	
+	return render_template('closure/index.html' , data=data , pagination = pagination)
+	#return render_template('login.html' )
+
 @closure.route('/create', methods = ['GET','POST'])
 def create():
 	"""
